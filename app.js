9f378162a03bdadc137d1b6017d699e49f8f2eb8
Ayers, Kevin Andrew <kayers8@gatech.edu>
2022-01-12 22:23:34 -0800

    setup add item and display alert functions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // ****** SELECT ITEMS **********
 const alert = document.querySelector('.alert');
 const form = document.querySelector('.grocery-form');
+// input 
 const grocery = document.getElementById('grocery');
 const submitBtn = document.querySelector('.submit-btn');
 const container = document.querySelector('.grocery-container');
@@ -13,8 +14,36 @@ let editFlag = false;
 let editID = "";
 
 // ****** EVENT LISTENERS **********
+// submit form
+form.addEventListener('submit', addItem);
 
 // ****** FUNCTIONS **********
+function addItem(event) {
+    event.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+
+    if (value && editFlag == false) {
+        //something
+    } else if (value && editFlag === true) {
+        // something else 
+    } else {
+       displayAlert("please enter value", "danger");
+    }  
+}
+
+// display alert
+function displayAlert(text, action) {
+    alert.textContent = text;
+    alert.classList.add(`alert-${action}`);
+
+
+    //remove alert
+    setTimeout(()=> {
+        alert.textContent = "";
+        alert.classList.remove(`alert-${action}`);
+    }, 1000)
+}
 
 // ****** LOCAL STORAGE **********
 
